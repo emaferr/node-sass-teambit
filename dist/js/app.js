556ecdf668d4cce64ddf2d6fee2e0854ffdcc464
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-14 22:30:20 +0200

    Corretto errore repository

--- dist/js/app.js
+++ dist/js/app.js
@@ -3056,7 +3056,6 @@ function within(min, value, max) {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
-alert('hello world');
 
 
 /***/ }),
